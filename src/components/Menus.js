c7aa3ae39e4effd6445d3e755ce2b093f2f184c4
Matheus Siqueira <matheussiqueira56@gmail.com>
2018-07-11 10:28:02 -0300

    Atualização do Menu

--- src/components/Menus.js
+++ src/components/Menus.js
@@ -3,13 +3,15 @@ import React from 'react';
 import '../App.css';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import Badge from '@material-ui/core/Badge';
-import {  Link } from 'react-router'
+import { Link } from 'react-router';
+import Typography from '@material-ui/core/Typography';
 
 function ButtonBases() {
     return (
         <div className="barraMenu">
             <img src={require('../logo.svg')} className="imagemMenu" alt="" />
             <div className="botaoHome">
+                <Typography variant="title" className="logo">Livraria Autbank</Typography>
                 <Button size="large">
                     <Link className="botaoLink1" to="/">Home</Link>
                 </Button>
